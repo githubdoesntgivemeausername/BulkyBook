67fbdff9d5e3a0939b0fd41a4502c44648763221
githubdoesntgivemeausername <aakashsukumar@gmail.com>
2023-01-03 22:15:56 +0530

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using BulkyBook.DataAccess.Repository.iRepository;//this statement was introduced by auto fix.may not be correct.
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+
+    {
+        void Update(Product obj);
+
+        
+    }
+}
